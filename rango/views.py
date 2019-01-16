1814fafb8b19eb6e02628aa6e5c4791693c962de
Pieris Kimonos <2319593k@student.gla.ac.uk>
2019-01-16 03:44:24 +0000

    Ch.4 - Added template index.html

--- rango/views.py
+++ rango/views.py
@@ -1,6 +1,15 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
+    # Construct a dictionary to pass to the template engine as its context.
+    # Note the key boldmessage is the same {{ boldmessage }} in the template!
+    context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
+
+    # Return a rendered response to send to the client.
+    # We make use of shortcut function to make our lives easier.
+    # Note that the first parameter is the tmeplate we wish to use.
+    return render(request, 'rango/index.html', context=context_dict)
     return HttpResponse("Rango says hey there partner! <br/> <a href='/rango/about/'>About</a>.")
 
 
