b86f290352edf6529052af33eeeac66fd839b9c4
Pieris Kimonos <2319593k@student.gla.ac.uk>
2019-01-17 03:21:49 +0000

    Ch.4 - Created template for about section

--- rango/views.py
+++ rango/views.py
@@ -10,8 +10,9 @@ def index(request):
     # We make use of shortcut function to make our lives easier.
     # Note that the first parameter is the tmeplate we wish to use.
     return render(request, 'rango/index.html', context=context_dict)
-    return HttpResponse("Rango says hey there partner! <br/> <a href='/rango/about/'>About</a>.")
-
 
 def about(request):
-    return HttpResponse("Rango says here is the about page. <br/> <a href='/rango/'>Index</a>.")
+
+    context_dict = {}
+
+    return render(request, 'rango/about.html', context=context_dict)
